fadfd87602d2cef773b811c84efb8e3d4beeaac1
Roger Rengifo <rogerrengifo@MacBook-Pro-de-Roger.local>
2020-08-09 20:52:01 -0500

    sequelize heroku

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
 const pg = require("pg");
 const { Sequelize } = require("sequelize");
 
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
+const db = new Sequelize(process.env.DATABASE_URL, {
+  host: "pagoinsibs-node-server",
   dialect: "postgres",
   pool: {
     max: 10,
