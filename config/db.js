579e22c7c5f907b15bbd48caed02ef2dbece3aad
Roger Rengifo <rogerrengifo@Rogers-MacBook-Pro.local>
2021-01-15 18:52:14 -0500

    Corregir funcion de conexion DB

--- config/db.js
+++ config/db.js
@@ -1,37 +1,37 @@
-const pg = require('pg');
-const { Sequelize } = require('sequelize');
+const pg = require("pg");
+const { Sequelize } = require("sequelize");
 
 let db;
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== "production") {
   db = new Sequelize(process.env.TEST_DB_URL, {
-    host: 'localhost',
-    dialect: 'postgres',
-    protocol: 'postgres',
+    host: "localhost",
+    dialect: "postgres",
+    protocol: "postgres",
     pool: {
       max: 7,
       min: 1,
       idle: 10000,
     },
     logging: false,
-    // dialectOptions: {
-    //   ssl: {
-    //     require: true,
-    //     rejectUnauthorized: false,
-    //   },
-    // },
   });
 } else {
   db = new Sequelize(process.env.DATABASE_URL, {
-    host: 'pagoinsibs-node-server',
-    dialect: 'postgres',
-    protocol: 'postgres',
+    host: "pagoinsibs-node-server",
+    dialect: "postgres",
+    protocol: "postgres",
     pool: {
       max: 7,
       min: 1,
       idle: 10000,
     },
     logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   });
 }
 
